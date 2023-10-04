0f774a64a7d8cd8f54c6ed0b38a8c31439a713ca
hp1heloisa <heloisapimentel2202@gmail.com>
2023-10-04 11:31:59 -0300

    aula6

--- aula-testes-06/tests/unit/orders.test.ts
+++ aula-testes-06/tests/unit/orders.test.ts
@@ -6,8 +6,22 @@ import { OrderInput } from "../../src/validator";
 
 describe("Order Service Tests", () => {
   it("should create an order", async () => {
-    jest.spyOn(createOrder, "")
-    expect(true).toBe(true);
+    const OrderInput: OrderInput = {
+      client: faker.person.fullName(),
+      description: faker.lorem.text()
+    }
+    jest.spyOn(orderRepository, "create").mockImplementationOnce((): any => {
+      return{
+        protocol: "oioi",
+        status: "oioi"
+      }
+    })
+    const order = await createOrder(OrderInput);
+    console.log(order);
+    expect(order).toEqual({
+      protocol: "oioi",
+      description: "oioi"
+    })
   });
 
   it("should return an order based on the protocol", async () => {
